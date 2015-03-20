69a46b196171c805ae14eb3e6dc7074a38770532
Ubuntu <ubuntu@ip-172-31-43-250.eu-west-1.compute.internal>
2015-03-20 20:38:47 +0000

    add task.json

--- doct/settings.py
+++ doct/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'doct.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_doct',
+        'USER': 'root',
+        'PASSWORD': 'patate41',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
